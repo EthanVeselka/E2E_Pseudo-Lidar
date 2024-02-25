5d597f6bba575830ff2c49fe0707e43a8e02b605
David Hatch <twevtwev21@gmail.com>
2024-02-24 21:13:02 -0600

    basic datagathering implemented

---
+++ carla_data/config.py
@@ -0,0 +1,5 @@
+CARLA_PYTHON_PATH = 'C:\my_workspace\school\csce482\carla_data\carla\WindowsNoEditor\PythonAPI\carla'
+DATA_PATH = 'C:\my_workspace\school\csce482\carla_data\data'
+POLL_RATE = 3
+CAMERA_X = 1920
+CAMERA_Y = 1080
