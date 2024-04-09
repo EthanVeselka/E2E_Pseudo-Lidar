099df2bf90998cebbb10f707a70b036a8b1ba4e9
Andrew Zehrer <andrewzehrer@tamu.edu>
2024-04-09 13:36:39 -0500

    removed pycache and my tests folder

--- PL_cli/tests/__main__.py
+++
@@ -1,7 +0,0 @@
-# # from test_cli import main as cli_main
-from test_edit_config import main as edit_config_main
-# # from test_model import main as model_main
-
-if __name__ == "__main__":
-    print("Running tests...")
-    edit_config_main()
