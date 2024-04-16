91ba9bd1cea658aa5ca2a1fc6199c5389537889b
EthanVeselka <ethan.veselka@gmail.com>
2024-04-16 16:17:25 -0500

    Added working split

--- models/FPN/scripts/fpn_requirements.sh
+++ models/FPN/scripts/fpn_requirements.sh
@@ -3,10 +3,10 @@
 # conda create --name e2e_pl python=3.6
 # conda activate e2e_pl
 # conda config --add channels conda-forge
-# python -m pip install tensorflow==1.4
-# python -m pip install opencv-python
-# python -m pip install pillow
-# python -m pip install scipy
+conda install tensorflow==1.4
+conda install opencv-python
+conda install pillow
+conda install scipy
 
 # Compile tf_ops
 cd ../models/tf_ops/3d_interpolation
